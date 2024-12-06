81b510ee094ff6cb3c60f3947ea25f9081c60099
ClareMeehan <161544979+ClareMeehan@users.noreply.github.com>
2024-12-06 12:57:53 -0500

    another update, data file and added aavs compatability in cleanup

--- functions.py
+++ functions.py
@@ -17,13 +17,16 @@ def cleanup(raw_data):
     Will cleanup the data file in order for better processing
     Includes: Outlier removal, get time to start from 0 (its in julian dates), and center the magnitude around the mean
     
-    Inputs: raw data file from OGLE (format = txt)
+    Inputs: raw data file from OGLE or from AAVS (can handle both formats)
     
     Returns: cleaned up data file
     '''
 
     # the format of the data is col 1: time, col 2: intensity
-    raw_data = np.loadtxt(raw_data)
+    if 'OGLE' in raw_data:
+        raw_data = np.loadtxt(raw_data)
+    if 'aavs' in raw_data:
+        raw_data = np.loadtxt(raw_data, skiprows = 1, delimeter = ',', usecols=(0,1))
     
     # get the times
     times = raw_data[:,0] - np.min(raw_data[:,0])   # sld be the first cols minus the starting time taking
@@ -31,9 +34,9 @@ def cleanup(raw_data):
     # first we want to do outlier removal for the intensity
     intensity = raw_data[:,1]
     # get quantiles
-    q1 = np.quantile(intensity,25)
-    q2 = np.quantile(intensity,50)
-    q3 = np.quantile(intensity,75)
+    q1 = np.quantile(intensity,.25)
+    q2 = np.quantile(intensity,.50)
+    q3 = np.quantile(intensity,.75)
     # get iqr range
     iqr = q3 - q1
     # get outlier bounds
@@ -48,7 +51,10 @@ def cleanup(raw_data):
     intensity = intensity - np.mean(intensity)
 
     # and now we return the cleaned up data as an array
-    return np.hstack((times,intensity))
+    return np.vstack((times,intensity)).T
+
+array = cleanup('cephied_001')
+print(array[:10,:])
 
 
 
