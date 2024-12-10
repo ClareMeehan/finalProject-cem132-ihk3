8a97502869f87e35bb4b088112ed4c60939c0e3c
ClareMeehan <161544979+ClareMeehan@users.noreply.github.com>
2024-12-09 19:47:08 -0500

    draft for the frequencies function done, and also changed the name of the cleanup function to Load_cleanup

--- functions.py
+++ functions.py
@@ -8,14 +8,16 @@
 
 # imports
 import numpy as np
-from scipy.signal import lombscargle
+import nfft
+import matplotlib.pyplot as plt
 
 # cleanup function
 
-def cleanup(raw_data):
+def Load_cleanup(raw_data):
     '''
     Will cleanup the data file in order for better processing
-    Includes: Outlier removal, get time to start from 0 (its in julian dates), and center the magnitude around the mean
+    Includes: Outlier removal, get time to start from 0 (its in julian dates)
+    and time normalization, and center the intensities around the mean
     
     Inputs: raw data file from OGLE
     
@@ -32,7 +34,6 @@ def cleanup(raw_data):
     intensity = raw_data[:,1]
     # get quantiles
     q1 = np.quantile(intensity,.25)
-    q2 = np.quantile(intensity,.50)
     q3 = np.quantile(intensity,.75)
     # get iqr range
     iqr = q3 - q1
@@ -44,9 +45,6 @@ def cleanup(raw_data):
     times = times[(intensity > lower) & (intensity < upper)]
     intensity = intensity[(intensity > lower) & (intensity < upper)]
 
-    # and now we center the data around the mean
-    intensity = intensity - np.mean(intensity)
-
     # normalize times to [-0.5, 0.5]
     t_norm = (times - np.min(times)) / (np.max(times) - np.min(times)) - 0.5
     # normalize intensities around 0
@@ -54,9 +52,54 @@ def cleanup(raw_data):
 
 
     # and now we return the cleaned up data as an array
-    return np.vstack((times,intensity)).T
+    return np.vstack((t_norm,i_norm,times,intensity)).T
+
+# now 
+def frequencies(data):
+    '''
+    Will take the clean data file in order to determine the max frequencies in it.
+    Includes an nonuniform fourier transform and the max frequency obtained from it.
+    
+    Inputs: the cleaned data file from OGLE (cleaned using cleanup function)
+    
+    Returns: the most probably frequency, the most probable period, the frequency spectrum
+    '''
+    # get the times and intensities that have been normalized
+    t_norm = data[:,0]
+    i_norm = data[:,1]
+    times = data[:,2]
+    
+    # get double the size
+    N = t_norm.size * 2
+    # and we want to make sure this is even
+    N += N % 2 != 0
+
+    # get the list of frequencies from the 
+    f_k = nfft.nfft_adjoint(t_norm, i_norm, N)
+    freqs = np.arange(-N//2, N//2)
+
+    # we really only care about the frequencies above a power of like 100
+    f_k[np.abs(f_k) <= 100] = 0
+
+    plt.plot(freqs, np.abs(f_k))
+    plt.savefig('power_graph.png')
+
+    # then we want to get the normalized period
+    T_recon_norm = -1 / freqs[np.argmax(np.abs(f_k))]
+    # and the unnormalized period
+    T_recon = T_recon_norm * (np.max(times) - np.min(times))
+
+    # we'll take the inverse of this to get the unnormlized frequency
+    max_f = 1 / T_recon
+
+    # and we return
+    return max_f, T_recon
 
 
+A = Load_cleanup('OGLE-BLG-CEP-001.dat')
+f, t = frequencies(A)
+print(t)
+    
 
 
 
